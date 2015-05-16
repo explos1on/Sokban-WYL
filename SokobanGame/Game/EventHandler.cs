35039810562a61e45f879bd80ea45807bb911382
AndrianoTasev <andrianotasev@gmail.com>
2015-05-16 19:32:35 +0300

    Added initial files.Everyone will choose one file and work on it.

---
+++ SokobanGame/Game/EventHandler.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game
+
+{
+    //Обработва и приема input и да го предава и на engine.
+    class EventHandler
+    {
+
+    }
+}
---
+++ SokobanGame/Game/GameEngine.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game
+{
+    //Тук е логиката на играта и от тук се управляват останалите класове.
+    class GameEngine
+    {
+    }
+}
